e8240a84f626ead88aa22b910d2074ceecb3c27c
soulduse <soulduse@naver.com>
2018-01-07 16:55:10 +0900

    Converted RepositoryActivity

--- simple-github/src/main/kotlin/com/androidhuman/example/simplegithub/ui/repo/RepositoryActivity.kt
+++ simple-github/src/main/kotlin/com/androidhuman/example/simplegithub/ui/repo/RepositoryActivity.kt
@@ -3,15 +3,12 @@ package com.androidhuman.example.simplegithub.ui.repo
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.ProgressBar
-import android.widget.TextView
 import com.androidhuman.example.simplegithub.R
 import com.androidhuman.example.simplegithub.api.GithubApi
-import com.androidhuman.example.simplegithub.api.GithubApiProvider
 import com.androidhuman.example.simplegithub.api.model.GithubRepo
+import com.androidhuman.example.simplegithub.api.provideGithubApi
 import com.androidhuman.example.simplegithub.ui.GlideApp
+import kotlinx.android.synthetic.main.activity_repository.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -21,49 +18,19 @@ import java.util.*
 
 class RepositoryActivity : AppCompatActivity() {
 
-    internal lateinit var llContent: LinearLayout
+    internal val api: GithubApi by lazy { provideGithubApi(this) }
 
-    internal lateinit var ivProfile: ImageView
+    internal var repoCall: Call<GithubRepo> ?= null
 
-    internal lateinit var tvName: TextView
-
-    internal lateinit var tvStars: TextView
-
-    internal lateinit var tvDescription: TextView
-
-    internal lateinit var tvLanguage: TextView
-
-    internal lateinit var tvLastUpdate: TextView
-
-    internal lateinit var pbProgress: ProgressBar
-
-    internal lateinit var tvMessage: TextView
-
-    internal lateinit var api: GithubApi
-
-    internal lateinit var repoCall: Call<GithubRepo>
-
-    internal var dateFormatInResponse = SimpleDateFormat(
+    internal val dateFormatInResponse = SimpleDateFormat(
             "yyyy-MM-dd'T'HH:mm:ssX", Locale.getDefault())
 
-    internal var dateFormatToShow = SimpleDateFormat(
+    internal val dateFormatToShow = SimpleDateFormat(
             "yyyy-MM-dd HH:mm:ss", Locale.getDefault())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_repository)
-
-        llContent = findViewById(R.id.llActivityRepositoryContent)
-        ivProfile = findViewById(R.id.ivActivityRepositoryProfile)
-        tvName = findViewById(R.id.tvActivityRepositoryName)
-        tvStars = findViewById(R.id.tvActivityRepositoryStars)
-        tvDescription = findViewById(R.id.tvActivityRepositoryDescription)
-        tvLanguage = findViewById(R.id.tvActivityRepositoryLanguage)
-        tvLastUpdate = findViewById(R.id.tvActivityRepositoryLastUpdate)
-        pbProgress = findViewById(R.id.pbActivityRepository)
-        tvMessage = findViewById(R.id.tvActivityRepositoryMessage)
-
-        api = GithubApiProvider.provideGithubApi(this)
 
         val login = intent.getStringExtra(KEY_USER_LOGIN) ?: throw IllegalArgumentException("No login info exists in extras")
         val repo = intent.getStringExtra(KEY_REPO_NAME) ?: throw IllegalArgumentException("No repo info exists in extras")
@@ -75,7 +42,7 @@ class RepositoryActivity : AppCompatActivity() {
         showProgress()
 
         repoCall = api.getRepository(login, repoName)
-        repoCall.enqueue(object : Callback<GithubRepo> {
+        repoCall!!.enqueue(object : Callback<GithubRepo> {
             override fun onResponse(call: Call<GithubRepo>, response: Response<GithubRepo>) {
                 hideProgress(true)
 
@@ -83,27 +50,27 @@ class RepositoryActivity : AppCompatActivity() {
                 if (response.isSuccessful && null != repo) {
                     GlideApp.with(this@RepositoryActivity)
                             .load(repo.owner.avatarUrl)
-                            .into(ivProfile)
+                            .into(ivActivityRepositoryProfile)
 
-                    tvName.text = repo.fullName
-                    tvStars.text = resources
+                    tvActivityRepositoryName.text = repo.fullName
+                    tvActivityRepositoryStars.text = resources
                             .getQuantityString(R.plurals.star, repo.stars, repo.stars)
-                    if (null == repo.description) {
-                        tvDescription.setText(R.string.no_description_provided)
+                    if (repo.description.isNullOrEmpty()) {
+                        tvActivityRepositoryDescription.setText(R.string.no_description_provided)
                     } else {
-                        tvDescription.text = repo.description
+                        tvActivityRepositoryDescription.text = repo.description
                     }
-                    if (null == repo.language) {
-                        tvLanguage.setText(R.string.no_language_specified)
+                    if (repo.language.isNullOrEmpty()) {
+                        tvActivityRepositoryLanguage.setText(R.string.no_language_specified)
                     } else {
-                        tvLanguage.text = repo.language
+                        tvActivityRepositoryLanguage.text = repo.language
                     }
 
                     try {
                         val lastUpdate = dateFormatInResponse.parse(repo.updatedAt)
-                        tvLastUpdate.text = dateFormatToShow.format(lastUpdate)
+                        tvActivityRepositoryLastUpdate.text = dateFormatToShow.format(lastUpdate)
                     } catch (e: ParseException) {
-                        tvLastUpdate.text = getString(R.string.unknown)
+                        tvActivityRepositoryLastUpdate.text = getString(R.string.unknown)
                     }
 
                 } else {
@@ -119,24 +86,31 @@ class RepositoryActivity : AppCompatActivity() {
     }
 
     private fun showProgress() {
-        llContent.visibility = View.GONE
-        pbProgress.visibility = View.VISIBLE
+        llActivityRepositoryContent.visibility = View.GONE
+        pbActivityRepository.visibility = View.VISIBLE
     }
 
     private fun hideProgress(isSucceed: Boolean) {
-        llContent.visibility = if (isSucceed) View.VISIBLE else View.GONE
-        pbProgress.visibility = View.GONE
+        llActivityRepositoryContent.visibility = if (isSucceed) View.VISIBLE else View.GONE
+        pbActivityRepository.visibility = View.GONE
     }
 
     private fun showError(message: String?) {
-        tvMessage.text = message?:"Error"
-        tvMessage.visibility = View.VISIBLE
+        with(tvActivityRepositoryMessage){
+            text = message?:"Error"
+            visibility = View.VISIBLE
+        }
+    }
+
+    override fun onStop() {
+        repoCall?.run { cancel() }
+        super.onStop()
     }
 
     companion object {
 
-        val KEY_USER_LOGIN = "user_login"
+        const val KEY_USER_LOGIN = "user_login"
 
-        val KEY_REPO_NAME = "repo_name"
+        const val KEY_REPO_NAME = "repo_name"
     }
 }
